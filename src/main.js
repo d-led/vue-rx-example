6ac9eeabcf128439ed45143fbab2c36237ecf0cb
Dmitry Ledentsov <dmlled@yahoo.com>
2019-01-29 22:30:02 +0100

    fixed Rx use

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRx from 'vue-rx'
+import Rx from 'rxjs/Rx'
 
 Vue.config.productionTip = false
 
-
+Vue.use(VueRx, Rx);
 
 new Vue({
   render: h => h(App)
